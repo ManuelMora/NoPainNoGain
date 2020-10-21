5c8218624d66b8053f6af5750cda2cd3c94d7c31
Manuel Mora <al_mora23@hotmail.com>
2020-10-21 10:42:38 -0500

    Enlasado el recurso sede con el servidor

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require('./src/server');
 const usuario = require('./src/usuario');
 const ciudad = require('./src/ciudad');
+const sede = require('./src/sede');
 
 server.agregarRecursoGet('/usuario/login', (req, res) => usuario.login(req, res));
 
 server.agregarRecursoPost('/ciudad/', (req, res) => ciudad.crearCiudad(req, res));
+server.agregarRecursoPost('/sede/', (req, res) => sede.crearSede(req, res));
 
 server.conectar();
